772b8dd6c4ba5c21446da378134423f57702431a
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-09-08 16:31:33 +0300

    feat: add profile drop-menu and modal

--- library/index.js
+++ library/index.js
@@ -7,7 +7,7 @@ const bodyStopScroll = document.querySelector(".body__wrapper");
 
 burger.addEventListener('click', function () {
     nav.classList.toggle('nav--visible');
-    burger.classList.toggle('burger--activ');
+    burger.classList.toggle('burger--active');
     body.classList.toggle('body--locked');
 })
 
@@ -23,49 +23,8 @@ navLink.forEach(function (el) {
 body.addEventListener('click', (event) => {
     if (!burger.contains(event.target)) {
         nav.classList.remove('nav--visible');
-        burger.classList.remove('burger--activ');
+        burger.classList.remove('burger--active');
         body.classList.remove('body--locked');
     }
 
 });
-
-
-// const paginationActiv = document.querySelectorAll('.pagination__button');
-
-
-// paginationActiv.forEach((el) => el.addEventListener('click', () => {
-//     el.classList.toggle('pagination__button--activ')
-// })
-// )
-
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: false,
-//     slidesPerView: 3,
-//     spaceBetween: 25,
-
-
-
-
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//     breakpoints: {
-//         0: {
-//             slidesPerView: 1,
-//         },
-//         769: {
-//             slidesPerView: 3,
-//         },
-
-//     }
-// });
--- library/profile.js
+++ library/profile.js
@@ -1,29 +1,60 @@
 const profileIcon = document.querySelector(".profile__icon")
 const profileMenu = document.querySelector(".profile__drop-menu")
 const bodyWrapper = document.querySelector(".body__wrapper")
-const profileButton = document.querySelector(".profile__drop-menu_button")
-const profileWord = document.querySelector(".profile_word")
-
+// const profileButton = document.querySelector(".profile__drop-menu_button")
+const profileReg = document.querySelector(".profile__menu_register")
+const register = document.querySelector(".register")
+// const registerActive = document.querySelector(".register--active")
+const regWrapper = document.querySelector(".register__wrapper");
 
 function showMenu() {
-    // при нажатии на иконку открывается модалка
+    // при нажатии на иконку открывается дроп-меню
     profileIcon.addEventListener('click', function (event) {
-        profileMenu.classList.toggle('profile__drop-menu--activ')
+        profileMenu.classList.toggle('profile__drop-menu--active')
         // console.log(event.target)
     })
     profileMenu.addEventListener('click', function (event) {
-        // при нажатии на кнопки рег и лог меню скрывается
-        // console.log(event.target.nodeName)
+        // при нажатии на кнопки рег и лог, дроп-меню скрывается
+        // console.log(event.target)
         if (event.target.nodeName == 'BUTTON') {
-            profileMenu.classList.toggle('profile__drop-menu--activ')
+            profileMenu.classList.toggle('profile__drop-menu--active')
         }
     })
-    bodyWrapper.addEventListener('click', (event) => {
+    // открытие окно регистрации
+    profileReg.addEventListener('click', function (e) {
+        register.classList.add('register--active')
 
-        if (!profileIcon.contains(event.target) && !profileMenu.contains(event.target)) {
-            profileMenu.classList.remove('profile__drop-menu--activ');
-        }
     })
+
+    bodyWrapper.addEventListener('click', (event) => {
+        // при нажатии на область вне дроп-меню, оно скрывается
+        if (!profileIcon.contains(event.target) && !profileMenu.contains(event.target)) {
+            profileMenu.classList.remove('profile__drop-menu--active');
+        }
+        // при нажатии на область вне окна регистрации, оно скрывается
+        if (!profileReg.contains(event.target) && !register.contains(event.target)) {
+            register.classList.remove('register--active')
+            // bodyWrapper.classList.remove('body__wrapper--opacity')
+            regWrapper.classList.add('register__wrapper-none')
+            // body.classList.remove('body__wrapper--locked')
+            // console.log(event.target)
+        }
+        // при открытом окне регистрации
+        if (register.classList.contains('register--active')) {
+            // bodyWrapper.classList.add('body__wrapper--opacity')
+            regWrapper.classList.remove('register__wrapper-none')
+            console.log('menu opened');
+        }
+        body.addEventListener('keydown', function (event) {
+            if (event.key == "Escape") {
+                register.classList.remove('register--active')
+                regWrapper.classList.add('register__wrapper-none')
+
+            }
+        })
+
+    })
+
 }
 showMenu()
 
