deb8a410019a2aba29683fe65fcc365b8d8bac65
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-09-23 21:41:30 +0300

    feat: add backward and forward buttons (Sat, Sep 23, 2023 09:41:30 PM)

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,17 +1,21 @@
 const audio = document.querySelector('audio');
 const play = document.querySelector(".play");
 const pause = document.querySelector(".pause");
-const main = document.querySelector("main")
-const buttons = document.querySelectorAll(".buttons")
+// const buttons = document.querySelectorAll(".buttons")
 const container = document.querySelector(".container")
+const musicName = document.querySelector(".music-name")
+const trecksNamesArr = ["Queen - The Show Must Go On", "Scorpions - Still Loving You", "Scorpions - Wind Of Change"]
+const backward = document.querySelector(".backward")
+const forward = document.querySelector(".forward")
 
-
+// function play
 function playAudio() {
 
     play.src = 'icons/Pause.svg';
     audio.play();
     container.classList.add('check')
 }
+// function pause
 function pauseAudio() {
 
     play.src = 'icons/Play.svg';
@@ -19,24 +23,37 @@ function pauseAudio() {
     container.classList.remove('check')
 }
 
-
-play.addEventListener('click', function (event) {
-    // playAudio();
-    ;
+// if play has clicked the pause button showing and music playing, and vise versa
+play.addEventListener('click', function () {
 
     if (container.classList.contains('check')) {
         pauseAudio()
     } else {
         playAudio()
     }
-    console.log(event.target);
-
 })
-// pause.addEventListener('click', function () {
-//     pauseAudio()
-// })
+
+let ind = 0;
+function nowPlaying(treck) {
+    musicName.innerHTML = treck;
+    audio.src = `audio/${treck}.mp3`
+}
+nowPlaying(trecksNamesArr[ind])
 
 
-
-
-
+forward.addEventListener('click', function () {
+    ind += 1;
+    if (ind > trecksNamesArr.length - 1) {
+        ind = 0;
+    }
+    nowPlaying(trecksNamesArr[ind])
+    playAudio()
+})
+backward.addEventListener('click', function () {
+    ind -= 1;
+    if (ind < 0) {
+        ind = trecksNamesArr.length - 1;
+    }
+    nowPlaying(trecksNamesArr[ind])
+    playAudio()
+})
