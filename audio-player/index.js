ec36aec88e72a780ccc9035c342faa2eb1592f2f
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-09-24 22:55:39 +0300

    feat: add rewind availability to progress-line (Sun, Sep 24, 2023 10:55:39 PM)

--- audio-player/index.js
+++ audio-player/index.js
@@ -2,11 +2,14 @@ const audio = document.querySelector('audio');
 const play = document.querySelector(".play");
 const pause = document.querySelector(".pause");
 const container = document.querySelector(".container")
-const musicName = document.querySelector(".music-name")
+let musicName = document.querySelector(".music-name")
 const trecksNamesArr = ["Queen - The Show Must Go On", "Scorpions - Still Loving You", "Scorpions - Wind Of Change"]
+const imagesArr = []
 const backward = document.querySelector(".backward")
 const forward = document.querySelector(".forward")
-const progressLine = document.querySelector(".progress-line")
+let progressLine = document.querySelector(".progress-line")
+const progressWrapper = document.querySelector(".progress__wrapper")
+const cover = document.querySelector(".cover")
 
 // function play
 function playAudio() {
@@ -19,8 +22,8 @@ function playAudio() {
 function pauseAudio() {
 
     play.src = 'icons/Play.svg';
-    audio.pause();
     container.classList.remove('check')
+    audio.pause();
 }
 
 // if play has clicked the pause button showing and music playing, and vise versa
@@ -37,6 +40,7 @@ let ind = 0;
 function nowPlaying(treck) {
     musicName.innerHTML = treck; // change music-name
     audio.src = `audio/${treck}.mp3` // change music path
+    cover.src = `images/${ind + 1}.jpg`
 
 }
 nowPlaying(trecksNamesArr[ind])
@@ -48,7 +52,13 @@ forward.addEventListener('click', function () {
         ind = 0;
     }
     nowPlaying(trecksNamesArr[ind])
-    playAudio()
+    // if a treck plaing already the next treck will playing too when forward button will clicked and conversly
+    if (container.classList.contains('check')) {
+        playAudio()
+    } else {
+        pauseAudio()
+    }
+
 })
 // if backward has clicked previous treck (trecksNamesArr[ind]) playing (playAudio())
 backward.addEventListener('click', function () {
@@ -57,18 +67,39 @@ backward.addEventListener('click', function () {
         ind = trecksNamesArr.length - 1;
     }
     nowPlaying(trecksNamesArr[ind])
-    playAudio()
+    // if a treck plaing already the previous treck will playing too when backward button will clicked and conversly
+    if (container.classList.contains('check')) {
+        playAudio()
+    } else {
+        pauseAudio()
+    }
 })
-progressLine.style.width = 0;
+
+
+
+progressLine.style.width = 0; // the default length of progress-line is 0
+
+// progress-line changes 
 audio.addEventListener('durationchange', function () {
     console.log('duration changed')
-    audio.addEventListener('timeupdate', function (event) {
-        console.log(event.target.duration)
-        console.log(event.target.currentTime);
 
-        progressLine.style.width = (event.target.currentTime * 100 / event.target.duration) + '%';
-    })
+
 })
-audio.addEventListener('loadedmetadata', function (event) {
-    console.log('meta loaded');
+// receiving timing of music playing
+audio.addEventListener('timeupdate', function (event) {
+    // console.log(event.target.duration)
+    // console.log(event.target.currentTime);
+
+    // set progress-line width 
+    progressLine.style.width = audio.currentTime * 100 / audio.duration + '%';
 })
+// click on the progress-line and set current position
+progressWrapper.addEventListener('click', function (event) {
+    console.log(event.target);
+    console.log(event.target.clientWidth)
+    console.log(event.offsetX);
+    // audio.currentTime should to get clicked position :)
+    audio.currentTime = (event.clientX - progressWrapper.getBoundingClientRect().left) / progressWrapper.clientWidth * audio.duration;
+
+})
+
