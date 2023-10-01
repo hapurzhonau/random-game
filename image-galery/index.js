0ee2f0d6891ffd951ea249fbc99c80a2e57f302e
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-10-01 18:03:12 +0300

    feat: add function for showing 10 ramdoms images when the page is reloaded (Sun, Oct 01, 2023 06:03:12 PM)

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,37 +1,39 @@
-const URL_RANDOM_UNSPLASH = "https://api.unsplash.com/photos/random?count=20";
 
 const submit = document.querySelector(".submit-button");
 const UNSPLASH_KEY = '2DpVpbIORVf8uhlkQBcv528EkQ4Gw8xIkP1e6SF0Bxs';
 const FLICKR_KEY = 'd4b5c2dff986acd52b12fe74ce0b8851';
 const form = document.querySelector(".form")
 const input = document.querySelector(".search__input")
-const imageWrapper = document.querySelector(".image__wrapper");
-const image = document.querySelector(".image")
+// const imageWrapper = document.querySelector(".image__wrapper");
+// const image = document.querySelector(".image")
 const imagesContainer = document.querySelector(".images__container")
+
 
 
 let inputQuery = "";
 
-function defaulter() {
-    const response = fetch(URL_RANDOM_UNSPLASH);
-    const responseJson = response.json();
-
+// function for showing 10 randoms images when the page is reloaded
+async function defaulter() {
+    const URL_RANDOM_UNSPLASH = `https://api.unsplash.com/photos/random?count=10&client_id=${UNSPLASH_KEY}`;
+    const response = await fetch(URL_RANDOM_UNSPLASH);
+    const responseJson = await response.json(); // in this case the 'response.json' is already array, becouse count=10 returns array
+    console.log(responseJson)
+    showImages(responseJson)
 }
-
+defaulter()
 
 // sending request and getting array by the Go! and Enter buttons
 submit.addEventListener('click', async function (event) {
 
     event.preventDefault();
-
+    imagesContainer.innerHTML = '';
     inputQuery = input.value;
 
     const URL_SEARCH = `https://api.unsplash.com/search/photos?query=${inputQuery}&client_id=${UNSPLASH_KEY}`;
 
     const response = await fetch(URL_SEARCH);
     const responseJson = await response.json();     // object
-    console.log(responseJson.results)       // data array
-    // const data = responseJson.results;
+    console.log(responseJson)       // data array
     showImages(responseJson.results) // this function has written below. it works with array 'responseJson.resutls'
 
 })
