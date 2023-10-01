d9e58588f762d41e24bb3460d23d5b2ee11e94c0
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-10-01 18:34:25 +0300

    refactor: add try/catch method and error message when search is failed (Sun, Oct 01, 2023 06:34:25 PM)

--- image-galery/index.js
+++ image-galery/index.js
@@ -4,8 +4,6 @@ const UNSPLASH_KEY = '2DpVpbIORVf8uhlkQBcv528EkQ4Gw8xIkP1e6SF0Bxs';
 const FLICKR_KEY = 'd4b5c2dff986acd52b12fe74ce0b8851';
 const form = document.querySelector(".form")
 const input = document.querySelector(".search__input")
-// const imageWrapper = document.querySelector(".image__wrapper");
-// const image = document.querySelector(".image")
 const imagesContainer = document.querySelector(".images__container")
 
 
@@ -15,10 +13,17 @@ let inputQuery = "";
 // function for showing 10 randoms images when the page is reloaded
 async function defaulter() {
     const URL_RANDOM_UNSPLASH = `https://api.unsplash.com/photos/random?count=10&client_id=${UNSPLASH_KEY}`;
-    const response = await fetch(URL_RANDOM_UNSPLASH);
-    const responseJson = await response.json(); // in this case the 'response.json' is already array, becouse count=10 returns array
-    console.log(responseJson)
-    showImages(responseJson)
+    try {
+        const response = await fetch(URL_RANDOM_UNSPLASH);
+        if (!response.ok) {
+            throw new Error('Random fetch response failed');
+        }
+        const responseJson = await response.json(); // in this case the 'response.json' is already array, becouse count=10 returns array
+        // console.log(responseJson)
+        showImages(responseJson)
+    } catch (error) {
+        console.error('Some error:', error)
+    }
 }
 defaulter()
 
@@ -30,12 +35,23 @@ submit.addEventListener('click', async function (event) {
     inputQuery = input.value;
 
     const URL_SEARCH = `https://api.unsplash.com/search/photos?query=${inputQuery}&client_id=${UNSPLASH_KEY}`;
+    try {
+        const response = await fetch(URL_SEARCH);
+        if (!response.ok) {
+            throw new Error('Search fetch response failed');
+        }
+        const responseJson = await response.json();     // object
+        // console.log(responseJson)       // data array
+        if (responseJson.results.length === 0) {
 
-    const response = await fetch(URL_SEARCH);
-    const responseJson = await response.json();     // object
-    console.log(responseJson)       // data array
-    showImages(responseJson.results) // this function has written below. it works with array 'responseJson.resutls'
-
+            const errorSpan = document.createElement("span");
+            errorSpan.textContent = "OOOPS! No results found.";
+            imagesContainer.appendChild(errorSpan);
+        }
+        showImages(responseJson.results) // this function has written below. it works with array 'responseJson.resutls'
+    } catch (error) {
+        console.error('Some error:', error)
+    }
 })
 
 
