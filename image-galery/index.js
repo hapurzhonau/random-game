e3b0c5f4e18123e69c448e51c26ad817c81bb62f
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-10-01 14:36:41 +0300

    feat: add function for showing images (one image) (Sun, Oct 01, 2023 02:36:41 PM)

--- image-galery/index.js
+++ image-galery/index.js
@@ -5,13 +5,16 @@ const UNSPLASH_KEY = '2DpVpbIORVf8uhlkQBcv528EkQ4Gw8xIkP1e6SF0Bxs';
 const FLICKR_KEY = 'd4b5c2dff986acd52b12fe74ce0b8851';
 const form = document.querySelector(".form")
 const input = document.querySelector(".search__input")
-const imagesDiv = document.querySelector(".images__item");
-// const image = document.querySelector("img")
+const imageWrapper = document.querySelector(".image__wrapper");
+const image = document.querySelector(".image")
+// const imagesContainer = document.querySelector(".images__container")
+
+
 let inputQuery = "";
 
 
 
-// sending request by the Go! button
+// sending request and getting array by the Go! and Enter buttons
 submit.addEventListener('click', async function (event) {
 
     event.preventDefault();
@@ -23,8 +26,17 @@ submit.addEventListener('click', async function (event) {
     const response = await fetch(URL_SEARCH);
     const responseJson = await response.json();     // object
     console.log(responseJson.results)       // data array
+    // const data = responseJson.results;
+    showImages(responseJson.results) // this function has written below. it works with array 'responseJson.resutls'
+
 })
 
-// function showImages(data) {
+// showing an images (one image)) 
+function showImages(arr) {
+    arr.map(function (el) {
+        image.src = el.urls.raw;
+        image.alt = el.alt_description;
 
-// }
+    })
+
+}
