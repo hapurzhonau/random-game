48596400550151b54848ed33ea976e3e4f5251bc
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-10-10 21:43:53 +0300

    feat: layout responsivness screen (Tue, Oct 10, 2023 09:43:53 PM)

--- random-game/index.js
+++ random-game/index.js
@@ -1,7 +1,18 @@
 const playField = document.querySelector(".playfield");
+const button = document.querySelector(".button");
+const playFieldTwo = document.querySelector(".playfield-two")
+
 playField.addEventListener('click', function (event) {
     let countedX = event.clientX - playField.getBoundingClientRect().left;
     let countedY = event.clientY - playField.getBoundingClientRect().top
     event.preventDefault()
     console.log('x: ', countedX, 'y: ', countedY)
+})
+function hitsCount() {
+    let count = 0;
+    // if(element)
+}
+button.addEventListener('click', function (event) {
+    playField.classList.toggle('invisible')
+    playFieldTwo.classList.toggle('invisible')
 })
